219d97caa6248969ee3b45ce08d0be52214ff1af
andrewsb108 <andrey.barsuk1@gmail.com>
2021-08-31 18:33:21 +0300

    Was created UserServiceTest

---
+++ src/test/java/com/servlet/project/model/service/UserServiceTest.java
@@ -0,0 +1,47 @@
+package com.servlet.project.model.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
